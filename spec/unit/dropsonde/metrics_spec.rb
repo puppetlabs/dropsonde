1019f76884d1e2514f4e97c4a2bd08ca6fc38924
Ben Ford <ben.ford@puppetlabs.com>
2020-06-11 12:34:39 -0700

    fix some specs

--- spec/unit/dropsonde/metrics_spec.rb
+++ spec/unit/dropsonde/metrics_spec.rb
@@ -1,16 +1,31 @@
 RSpec.describe Dropsonde::Metrics do
 
-  it "loads list of plugins" do
-    expect(Dropsonde::Metrics.plugins.size).to eq(Dir.glob('lib/dropsonde/metrics/*').size)
+  context "default" do
+    it "loads list of plugins" do
+      expect(Dropsonde::Metrics.plugins.size).to eq(Dir.glob('lib/dropsonde/metrics/*').size)
+    end
+
+    # validates each plugin's description
+    it "generates example data" do
+      expect(subject.list).to be_a(String)
+    end
+
+    # validates each plugin's schema
+    it "generates a schema" do
+      expect(subject.schema).to be_an(Array)
+    end
+
+    # validates each plugin's example
+    it "generates example data" do
+      expect(subject.example).to be_a(Hash)
+    end
   end
 
-#   it "disables plugins" do
-#     Dropsonde.settings[:disable] = [:puppetfiles]
-#     expect(Dropsonde::Metrics.plugins.size).to eq(Dir.glob('lib/dropsonde/metrics/*').size - 1)
-#   end
+  context "disabled plugins" do
+    Dropsonde.settings[:disable] = [:puppetfiles]
 
-  it "generates a schema" do
-    expect(subject.schema).to be_an(Array)
+    it "disables plugins" do
+      expect(Dropsonde::Metrics.plugins.size).to eq(Dir.glob('lib/dropsonde/metrics/*').size - 1)
+    end
   end
-
 end
