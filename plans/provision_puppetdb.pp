0cfd1648fcd3e7b0f2214e1fbedde00bb6271422
adrianiurca <adrian.iurca@gmail.com>
2021-07-21 04:57:21 +0300

    fix 2

--- plans/provision_puppetdb.pp
+++ plans/provision_puppetdb.pp
@@ -7,13 +7,16 @@ plan dropsonde::provision_puppetdb(
     $postgres = get_targets('*').filter |$n| { $n.vars['role'] == 'postgres' }
     $puppetserver_ip = run_command('facter networking.interfaces.eth0.ip', $puppetserver).to_data[0]['value']['stdout'].strip()
     $postgres_ip = run_command("getent hosts postgres | awk '{ print \$1 }'", $postgres).to_data[0]['value']['stdout'].strip()
+    $puppetserver_id = facts($puppetserver[0])['container_id']
+    $postgres_id = facts($postgres[0])['container_id']
     $env_vars = "'-e', 'PUPPETDB_POSTGRES_HOSTNAME=${postgres_ip}', '-e', 'PUPPETSERVER_HOSTNAME=${puppetserver_ip}'"
+    $links = "'--link', '${postgres_id}:postgres', '--link', '${puppetserver_id}:puppet'"
     run_task(
       "provision::${using}",
       'localhost',
       action => 'provision',
       platform => $image,
-      vars => "role: puppetdb\ndocker_run_opts: ['--hostname', 'puppetdb', ${env_vars}]"
+      vars => "role: puppetdb\ndocker_run_opts: ['--hostname', 'puppetdb', ${env_vars}, ${links}]"
     )
   } else {
     run_task(
